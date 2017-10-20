0a5be720f57f1cddbbac404dd3e8b258d43d3dc2
Quentin Deldycke <quentindeldycke@gmail.com>
2017-10-20 11:32:23 +0200

    - adding logs

--- main.cpp
+++ main.cpp
@@ -4,14 +4,17 @@
 
 int main(int argc, char *argv[])
 {
+    qDebug() << "START OF MAIN!";
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
     nfc::getInstance().startNfc();
     engine.load(QUrl(QLatin1String("qrc:/main.qml")));
+    qDebug() << "Loaded qml!";
     if (engine.rootObjects().isEmpty())
         return -1;
-
-    return app.exec();
+    int x = app.exec();
+    qDebug() << "END OF MAIN";
+    return x;
 }
